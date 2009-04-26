rofl0r/ncdu

commit ece21a668d43dac69c25315a22f1f7d0147f7198

Fixed display of the root directory

src/calc.c
@@ -344,9 +344,13 @@ void calc_process() {
     strcpy(t->name, orig->name);
   } else {
     t->name = malloc(strlen(path)+strlen(name)+1);
-    strcpy(t->name, path);
-    strcat(t->name, "/");
-    strcat(t->name, name);
+    if(strcmp(path, "/"))
+      strcpy(t->name, path);
+    if(strcmp(name, ".")) {
+      strcat(t->name, "/");
+      strcat(t->name, name);
+    } else
+      t->name[0] = 0;
   }
   root = t;
   curdev = fs.st_dev;

src/util.c
@@ -214,6 +214,9 @@ char *getpath(struct dir *cur) {
   struct dir *d, **list;
   int c, i;
 
+  if(!cur->name[0])
+    return "/";
+
   c = i = 1;
   for(d=cur; d!=NULL; d=d->parent) {
     i += strlen(d->name)+1;

commit 2a5c9a242f3af10e90ef8a22cc21758dda8a61a4

Fixed display of one-component-after-root directory
Bug introduced in ece21a668d43dac69c25315a22f1f7d0147f7198
It's amazing how many mistakes one person can make in a few hours.

src/calc.c
@@ -344,13 +344,13 @@ void calc_process() {
     strcpy(t->name, orig->name);
   } else {
     t->name = malloc(strlen(path)+strlen(name)+1);
+    t->name[0] = 0;
     if(strcmp(path, "/"))
       strcpy(t->name, path);
     if(strcmp(name, ".")) {
       strcat(t->name, "/");
       strcat(t->name, name);
-    } else
-      t->name[0] = 0;
+    }
   }
   root = t;
   curdev = fs.st_dev;