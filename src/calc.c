rofl0r/ncdu

commit 399ccdeb47b9ec659a3137fbe4270cf6d273730b

calc.c: Fix path display when scanning an empty directory
http://dev.yorhel.nl/ncdu/bug/15

src/calc.c
@@ -458,11 +458,6 @@ int calc_process() {
   /* calculate */
   n = calc_dir(root, name);
 
-  /* free some resources */
-  if(!path[1] && strcmp(name, "."))
-    free(name);
-  free(path);
-
   if(links) {
     kh_destroy(hl, links);
     links = NULL;
@@ -473,6 +468,7 @@ int calc_process() {
     if(root->sub == NULL) {
       freedir(root);
       failed = 1;
+      calc_enterpath(name);
       strcpy(errmsg, "Directory empty.");
       goto calc_fail;
     }
@@ -498,6 +494,10 @@ int calc_process() {
   /* something went wrong... */
   freedir(root);
 calc_fail:
+  if(!path[1] && strcmp(name, "."))
+    free(name);
+  free(path);
+
   while(failed && !input_handle(0))
     ;
   if(orig == NULL)

commit 0b49021a6c676975d93c83e1e4228f96136c334c

399ccdeb caused a crash on non-existent directories on the command line

src/calc.c
@@ -367,7 +367,7 @@ int calc_key(int ch) {
 
 
 int calc_process() {
-  char *path, *name;
+  char *path = NULL, *name = NULL;
   struct stat fs;
   struct dir *t;
   int n;
@@ -494,7 +494,7 @@ int calc_process() {
   /* something went wrong... */
   freedir(root);
 calc_fail:
-  if(!path[1] && strcmp(name, "."))
+  if(name && path && !path[1] && strcmp(name, "."))
     free(name);
   free(path);
 