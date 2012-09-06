fdc614126a7284cb5408d273a9735e9eabc3a2e6
Yorhel <git@yorhel.nl>
2012-09-06 12:32:41 +0200

    browser.c: Display [imported] indication in title

--- src/browser.c
+++ src/browser.c
@@ -162,7 +162,9 @@ void browse_draw() {
   mvhline(0, 0, ' ', wincols);
   mvhline(winrows-1, 0, ' ', wincols);
   mvprintw(0,0,"%s %s ~ Use the arrow keys to navigate, press ? for help", PACKAGE_NAME, PACKAGE_VERSION);
-  if(read_only)
+  if(dir_import_active)
+    mvaddstr(0, wincols-10, "[imported]");
+  else if(read_only)
     mvaddstr(0, wincols-11, "[read-only]");
   attroff(A_REVERSE);
 
